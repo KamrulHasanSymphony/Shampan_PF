792a78dae779560aee7424c8203d04342403978f
DEV-1007\Torekul <torekul.islam@symphonysoftt.com>
2026-03-08 13:41:11 +0600

    dsds

--- SymWebNew/Areas/PF/Controllers/PFReportController.cs
+++ SymWebNew/Areas/PF/Controllers/PFReportController.cs
@@ -2226,7 +2226,7 @@ namespace SymWebUI.Areas.PF.Controllers
                  doc.DataDefinition.FormulaFields["DateTo"].Text = "'" + vm.DateTo + "'";
                  doc.DataDefinition.FormulaFields["Address"].Text = "'" + cvm.Address + "'";
                  doc.DataDefinition.FormulaFields["CompanyName"].Text = "'" + cvm.Name + "'";
-                 //doc.DataDefinition.FormulaFields["BranchName"].Text = "'" + Session["BranchName"].ToString() + "'";
+                 doc.DataDefinition.FormulaFields["BranchName"].Text = "'" + Session["BranchName"].ToString() + "'";
 
                  //doc.DataDefinition.FormulaFields["frmGroupBy"].Text = "'" + groupBy + "'";
                  var rpt = RenderReportAsPDF(doc);
