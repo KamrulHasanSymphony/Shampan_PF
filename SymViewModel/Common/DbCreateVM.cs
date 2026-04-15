3be6f6341878527779d585c0fda42c45d27532d7
DEV-1008\Perves <pipul.rahman@symphonysoftt.com>
2026-04-15 16:56:14 +0600

    DB Create

---
+++ SymViewModel/Common/DbCreateVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace SymViewModel.Common
+{
+    public class DbCreateVM
+    {
+        public string Id { get; set; }
+        [Display(Name = "Log IN")] 
+        public string LogIN { get; set; }
+        public string Password { get; set; }
+        [Display(Name = "Server Name")] 
+        public string ServerName { get; set; }
+        [Display(Name = "Database Name")]
+        public string DatabaseName { get; set; }
+    }
+}
